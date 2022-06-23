6d49efa8b3589650b319f7272062000656cf7aed
Pedro Torres <torrespedrobarba@gmail.com>
2022-06-23 14:22:38 +0200

    Añadido alert con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Web_Bravekey';
+
+  ngOnInit() {
+    var num1 = 10
+    var num2 = 2
+    const hora = new Date();
+
+    window.alert("Bienvenido a la landing page de Bravekey, realizada con Angular y Sass.\n" + 
+    "Para demostrar el funcionamiento de Angular, puedes ver estas 2 variables: " + num1 + " y " + num2 + ". La suma de ambas es: " + (num1 + num2) + "\n\n" +
+    "La hora actual es: " + hora)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+
+}
